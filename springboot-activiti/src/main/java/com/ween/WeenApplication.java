539625b10f3b6f3b678bc6d2c6dca04019903451
weenhall <843506936@qq.com>
2018-07-30 14:46:40 +0800

    first commit

---
+++ springboot-activiti/src/main/java/com/ween/WeenApplication.java
@@ -0,0 +1,12 @@
+package com.ween;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeenApplication.class, args);
+	}
+}
