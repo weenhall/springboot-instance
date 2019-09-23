0919531deb779eb7dc8fc90784471b27dd88a0a6
weenhall <843506936@qq.com>
2019-09-23 09:36:53 +0800

    spring-kafka module

---
+++ springboot-kafka/src/main/java/com/example/springbootkafka/procedure/Message.java
@@ -0,0 +1,29 @@
+package com.example.springbootkafka.procedure;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by ween on 2019/3/21
+ */
+public class Message {
+    private int type;
+    private List<?> list;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public List<?> getList() {
+        return list;
+    }
+
+    public void setList(List<?> list) {
+        this.list = list;
+    }
+}
---
+++ springboot-kafka/src/test/java/com/example/springbootkafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,29 @@
+package com.example.springbootkafka;
+
+import com.example.springbootkafka.consumer.ConsumerDemo;
+import com.example.springbootkafka.procedure.Producer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootKafkaApplicationTests {
+
+	@Autowired
+	private Producer producer;
+
+	@Autowired
+	private ConsumerDemo consumerDemo;
+
+	@Test
+	public void send() throws InterruptedException {
+		for(int i=0;i<5;i++){
+			producer.send(i);
+			Thread.sleep(1000);
+		}
+	}
+
+}
