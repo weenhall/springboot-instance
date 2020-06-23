a73f9f35bc5593c1beede7dbcf835dba51e92d76
weenhall <843506936@qq.com>
2020-06-23 15:44:27 +0800

    add springboot-session

--- springboot-session/src/main/java/com/example/demo/SpringbootSession.java
+++ springboot-session/src/main/java/com/example/demo/SpringbootSession.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringbootSession {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringbootSession.class, args);
 	}
 
 }
