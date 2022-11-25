1e59636382818d210a37e9777985379d9668519b
weenhall <weenhallwu@gmail.com>
2022-11-25 14:59:10 +0800

    add activiti h2 profile

--- springboot-activiti/src/main/java/com/example/springbootactiviti/config/ActivitiConfig.java
+++ springboot-activiti/src/main/java/com/example/springbootactiviti/config/ActivitiConfig.java
@@ -1,10 +1,8 @@
 package com.example.springbootactiviti.config;
 
 import com.example.springbootactiviti.ext.CustomGroupEntityManager;
-import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.spring.SpringProcessEngineConfiguration;
 import org.activiti.spring.boot.ProcessEngineConfigurationConfigurer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
